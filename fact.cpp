2b1788ea8598ad6c043797b4a89e7eb8858bb1e2
nitish091 <nitishrawat009@gmail.com>
2020-05-21 02:30:38 +0530

    fact.cpp: changed to hello world program

--- fact.cpp
+++ fact.cpp
@@ -1,22 +1,6 @@
 #include<iostream>
-#define ll unsigned long long int
 using namespace std;
-ll Fib( ll N ){
-    if( N > 2 ){
-        return ( Fib( N-1 ) + Fib( N-2 ) );
-    }
-    else if( N == 2 ){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-
-}
 int main(){
-	ll i;
-    for( i = 1 ; i <= 60 ; i++){
-        cout <<Fib(i)%10<<"\t"<<Fib(60 + i)%10<< endl;
-    }
+    cout << "Hello World" << endl;
     return 0;
 }
